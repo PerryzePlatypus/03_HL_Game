db9c6fcdc1eb953a287881151f800a55f3c63b8f
PerryzePlatypus <gonzalesp4476@masseyhigh.school.nz>
2026-03-25 11:39:47 +1300

    No duplicated guesses

---
+++ no_duplicate.py
@@ -0,0 +1,24 @@
+
+
+already_guessed = []
+
+secret = 7
+guesses_used = 0
+guesses_allowed = 5
+
+
+
+
+guess = ""
+while guess != secret:
+    guess = int(input("Guess: "))
+    if guess in already_guessed:
+        print(f"You've already guessed {guess}. You've *still* used"
+              f" {guesses_used} / {guesses_allowed} guesses")
+        continue
+
+    else:
+        already_guessed.append(guess)
+
+    guesses_used +=1
+
